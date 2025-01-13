501f1712734c30b2f6e0ae278c5b68d715a474c0
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-13 13:40:16 +0600

    Update url.py

--- src_pyloid/bridge/url.py
+++ src_pyloid/bridge/url.py
@@ -9,4 +9,4 @@ import os
 class URL(PyloidAPI):
     @Bridge(result=str)
     def get_production_path(self):
-        return str(os.path.join(get_production_path(), "frontend-dist"))
+        return str(os.path.join(get_production_path(), "dist-front"))
