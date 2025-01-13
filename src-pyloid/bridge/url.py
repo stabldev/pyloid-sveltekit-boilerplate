42660e05a827d9651ec24fc0483c3f709df2a527
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-13 12:41:17 +0600

    A

---
+++ src-pyloid/bridge/url.py
@@ -0,0 +1,12 @@
+from pyloid import (
+    PyloidAPI,
+    Bridge,
+    get_production_path,
+)
+import os
+
+
+class JSApi(PyloidAPI):
+    @Bridge(result=str)
+    def get_production_path(self):
+        return str(os.path.join(get_production_path(), "frontend-dist"))
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { type Snippet } from 'svelte';
+
+    let base = $state('');
+    onMount(async () => {
+        base = (await window.pyloid.JSApi.get_production_path()) || '';
+    });
+
+    const { children }: { children: Snippet } = $props();
+</script>
+
+<svelte:head>
+    <base href={base} />
+</svelte:head>
+
+{@render children?.()}
