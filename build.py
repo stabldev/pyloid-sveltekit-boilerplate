23fd7ecbebd01134eed930c5ec669f9969a93c87
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-01-13 08:09:53 +0530

    chore: add back pyloid vite build scripts

--- build.py
+++ build.py
@@ -3,9 +3,7 @@ from pyloid.builder import create_spec_from_json, build_from_spec, cleanup_befor
 
 def main():
     spec_path = create_spec_from_json("build_config.json")
-
     cleanup_before_build("build_config.json")
-
     build_from_spec(spec_path)
 
 
