2c527f8e84c202630896994bae96bbb3fe8337ae
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-14 21:28:15 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,6 @@
   import { type Snippet } from 'svelte';
 
   const { children }: { children: Snippet } = $props();
-  let base = $state('');
-
-  onMount(async () => {
-    base = (await window.pyloid.JSApi.get_production_path()) || '';
-  });
 </script>
 
 {@render children()}
