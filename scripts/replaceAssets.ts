96402871b07887dfed1cfa900d895ee0e40afc68
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-22 10:55:07 +0600

    Do formatting

--- scripts/replaceAssets.ts
+++ scripts/replaceAssets.ts
@@ -8,34 +8,34 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 const readDirRecursive = async (filePath: string): Promise<string[]> => {
-  const dir = await fs.promises.readdir(filePath);
-  const files = await Promise.all(
-    dir.map(async (relativePath) => {
-      const absolutePath = path.join(filePath, relativePath);
-      const stat = await fs.promises.lstat(absolutePath);
-      return stat.isDirectory() ? readDirRecursive(absolutePath) : absolutePath;
-    })
-  );
-  return files.flat();
+    const dir = await fs.promises.readdir(filePath);
+    const files = await Promise.all(
+        dir.map(async (relativePath) => {
+            const absolutePath = path.join(filePath, relativePath);
+            const stat = await fs.promises.lstat(absolutePath);
+            return stat.isDirectory() ? readDirRecursive(absolutePath) : absolutePath;
+        })
+    );
+    return files.flat();
 };
 
 const files = await readDirRecursive('./dist-front');
 if (files) {
-  Array.from(files).forEach((file) => {
-    if (
-      !(
-        file.endsWith('.js') ||
-        file.endsWith('.html') ||
-        file.endsWith('.map') ||
-        file.endsWith('.css')
-      )
-    ) {
-      return;
-    }
-    fs.readFile(file, 'utf8', (_, data) => {
-      fs.writeFile(file, data.replace(/http:\/\/<REPLACEME>/g, '.'), 'utf8', () => {
-        console.log("Wrote file '" + file + "'");
-      });
+    Array.from(files).forEach((file) => {
+        if (
+            !(
+                file.endsWith('.js') ||
+                file.endsWith('.html') ||
+                file.endsWith('.map') ||
+                file.endsWith('.css')
+            )
+        ) {
+            return;
+        }
+        fs.readFile(file, 'utf8', (_, data) => {
+            fs.writeFile(file, data.replace(/http:\/\/<REPLACEME>/g, '.'), 'utf8', () => {
+                console.log("Wrote file '" + file + "'");
+            });
+        });
     });
-  });
 }
