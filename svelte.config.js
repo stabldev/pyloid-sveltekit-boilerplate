d4dff22839a620a9f8c6616ab96bcc0d111f9612
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-01-22 11:16:33 +0600

    chore: import node (#8)
    * Add
    
    * Update main.py
    
    * Hmm
    
    * ADD
    
    * Update +layout.svelte
    
    * Update main.py
    
    * HJ
    
    * HJ
    
    * Add
    
    * Add
    
    * Fix
    
    * Add
    
    * Add
    
    * Update replaceAssets.ts
    
    * Update package.json
    
    * Update package.json
    
    * Revert "Merge remote-tracking branch 'origin/main' into init"
    
    This reverts commit 8f25527f5075dcdfc244063812dd0f4aff7197b7, reversing
    changes made to 7bae8eec9fee25f70520b3406a23873ada3740cf.
    
    * Do formatting

--- svelte.config.js
+++ svelte.config.js
@@ -3,27 +3,27 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  compilerOptions: {
-    runes: true
-  },
-
-  kit: {
-    adapter: adapter({
-      pages: 'dist-front',
-      strict: true,
-      fallback: '404.html'
-    }),
-    paths: {
-      assets: 'http://<REPLACEME>',
-      relative: true
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+    compilerOptions: {
+        runes: true
     },
-    router: {
-      type: 'hash'
+
+    kit: {
+        adapter: adapter({
+            pages: 'dist-front',
+            strict: true,
+            fallback: '404.html'
+        }),
+        paths: {
+            assets: 'http://<REPLACEME>',
+            relative: true
+        },
+        router: {
+            type: 'hash'
+        }
     }
-  }
 };
 
 export default config;
